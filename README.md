d19f65c036ccac515e4f6fda642f6f8ee9953815
99Ace <ace.ang@trentglobal.edu.sg>
2022-03-14 16:41:29 +0000

    update more links

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # 99Ace-api-mikar9
+
+    DATASET
+    {
+        first_name : first_name,
+        last_name : last_name,
+        position : position
+    }
+    
+    // READ - ALL DATA
+    app.get('/m9User/read')
+
+    // CREATE - NEW DATA
+    app.post('/m9User/create'
+    
+    // UPDATE - ONE DATA
+    app.put("/m9User/:id/update"
+
+    // DELETE - ONE DATA
+    app.delete("/m9User/:id/delete"
+
+    // FIND ONE - ONE DATA
+    app.get('/m9User/:id/findone
+
+    // SEARCH FOR FIRST_NAME
+    app.get('/m9User/search/:first_name/first_name'
+
+    // SEARCH FOR LAST_NAME
+    app.get('/m9User/search/:last_name/last_name'
+
+    // SEARCH FOR POSITION
+    app.get('/m9User/search/:position/position
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const MongoUtil = require("./MongoUtil.js");
 const ObjectId = require('mongodb').ObjectId;
 const Mail = require('nodemailer/lib/mailer');
-const cors = require("cors")
+const cors = require("cors");
+const async = require('hbs/lib/async');
 require('dotenv').config()
 
 async function main() {
@@ -15,7 +16,7 @@ async function main() {
 
     // ROUTES
     // READ - ALL DATA
-    app.get('/m9User', async (req,res)=> {
+    app.get('/m9User/read', async (req,res)=> {
         try {
             let data = await db.collection(process.env.COLLECTION).find().toArray();
             res.status(200)
@@ -30,7 +31,7 @@ async function main() {
         }
     })
     // CREATE - NEW DATA
-    app.post('/m9User', async (req,res)=> {
+    app.post('/m9User/create', async (req,res)=> {
         let first_name = req.body.first_name;
         let last_name = req.body.last_name; 
         let position = req.body.position;
@@ -53,7 +54,7 @@ async function main() {
         }
     })
     // UPDATE - ONE DATA
-    app.put("/m9User/:id", async (req,res)=>{
+    app.put("/m9User/:id/update", async (req,res)=>{
         try{
             let results = await db.collection(process.env.COLLECTION).updateOne(
             {
@@ -82,7 +83,7 @@ async function main() {
     })
 
     // DELETE - ONE DATA
-    app.delete("/m9User/:id", async(req,res)=>{
+    app.delete("/m9User/:id/delete", async(req,res)=>{
         try {
             await db.collection(process.env.COLLECTION).deleteOne({
                 _id:ObjectId(req.params.id)
@@ -100,9 +101,82 @@ async function main() {
             console.log(e);
         }
     })
+    // FIND ONE - ONE DATA
+    app.get('/m9User/:id/findone', async (req,res)=>{
+        try {
+            let data = await db.collection(process.env.COLLECTION).find({
+                "_id" : ObjectId(req.params.id)
+            }).toArray();
+            res.status(200)
+            res.send(data[0])
+            console.log('data sent')
+        } 
+        catch(e)  {
+            res.status(500);
+            res.send({
+                message : "No data available"
+            })
+        }
+    })
+    // SEARCH FOR FIRST_NAME
+    app.get('/m9User/search/:first_name/first_name', async (req,res)=>{
+        try {
+            let data = await db.collection(process.env.COLLECTION).find({
+                "first_name" : req.params.first_name,
+            }).toArray();
+            res.status(200)
+            res.send(data)
+            console.log('data sent')
+        } 
+        catch(e)  {
+            res.status(500);
+            res.send({
+                message : "No data available"
+            })
+        }
+    })
+    // SEARCH FOR LAST_NAME
+    app.get('/m9User/search/:last_name/last_name', async (req,res)=>{
+        console.log(req.params.last_name)
+        try {
+            let data = await db.collection(process.env.COLLECTION).find({
+                "last_name" : req.params.last_name
+            }).toArray();
+            res.status(200)
+            res.send(data)
+            console.log('data sent')
+        } 
+        catch(e)  {
+            res.status(500);
+            res.send({
+                message : "No data available"
+            })
+        }
+    })
+    // SEARCH FOR POSITION
+    app.get('/m9User/search/:position/position', async (req,res)=>{
+        try {
+            let data = await db.collection(process.env.COLLECTION).find({
+                "position" : req.params.position
+            }).toArray();
+            res.status(200)
+            res.send(data)
+            console.log('data sent')
+        } 
+        catch(e)  {
+            res.status(500);
+            res.send({
+                message : "No data available"
+            })
+        }
+    })
 
     app.listen(process.env.PORT, () => {
         console.log("Server started")
     })
+    // app.listen(3000, () => {
+    //     console.log("Server started")
+    // })
+    
 }
 main()
